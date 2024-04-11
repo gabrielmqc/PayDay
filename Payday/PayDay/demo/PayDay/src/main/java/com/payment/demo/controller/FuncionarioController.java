e7a3d0a8ea027d27f9936910bc5f8ce50542d688
Gabriel Mazzo <gabriel_mqc@hotmail.com>
2024-04-10 22:10:47 -0300

    ops

--- Payday/PayDay/demo/PayDay/src/main/java/com/payment/demo/controller/FuncionarioController.java
+++ Payday/PayDay/demo/PayDay/src/main/java/com/payment/demo/controller/FuncionarioController.java
@@ -1,31 +1,31 @@
 package com.payment.demo.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.payment.demo.model.Funcionario;
 import com.payment.demo.service.FuncionarioService;
 
 @RestController
-@RequestMapping(value = "/funcionario")
+@RequestMapping("/funcionarios")
 public class FuncionarioController {
 
     @Autowired
     private FuncionarioService funcionarioService;
-    private Funcionario funcionario;
-    
-    @GetMapping
-    public FuncionarioService exibirFuncionario(){
-        return funcionarioService;
-    }
-    @PostMapping
-    public void adicionarEvento(@RequestBody FuncionarioService funcionarioService, Funcionario funcionario) {
-        funcionarioService.executarEvento(funcionario.getTipoEvento());
-    }
 
-    
+    /*
+     @GetMapping("/funcionarios")
+    public List<Funcionario> listar() {
+      return funcionarioService.carregarFuncionarios();
+    }
+  
+*/
 }
--- Payday/PayDay/demo/PayDay/src/main/java/com/payment/demo/service/FuncionarioService.java
+++ Payday/PayDay/demo/PayDay/src/main/java/com/payment/demo/service/FuncionarioService.java
@@ -1,81 +1,104 @@
-    package com.payment.demo.service;
+package com.payment.demo.service;
 
-    import org.springframework.beans.factory.annotation.Autowired;
-    import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.payment.demo.model.Dependente;
 import com.payment.demo.model.Eventos;
 import com.payment.demo.model.Funcionario;
+import java.io.IOException;
 
-    @Service
-    public class FuncionarioService {
-        
-        @Autowired
-        private EventosService eventosService;
-        
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import java.io.File;
 
-        public void calcularSalarioLiquido(Funcionario funcionario) {
-            // Resetar o valor total dos eventos antes de calcular
-            eventosService.resetarValorTotalEventos();
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
-            // Executar eventos para o funcionário
-            eventosService.adicionarHoraExtra50(funcionario.getNivelSalarial(), funcionario.getQuantiaHoraExtra50());
-            eventosService.adicionarHoraExtra100(funcionario.getNivelSalarial(), funcionario.getQuantiaHoraExtra100());
-            eventosService.subtrairFaltas(funcionario.getNivelSalarial(), funcionario.getQuantiaFaltas());
-            eventosService.descontoPorAtraso(funcionario.getNivelSalarial(), funcionario.getQuantiaDescontoPorAtraso());
-            eventosService.descansoSemanalRemunerado(funcionario.getNivelSalarial(), funcionario.getQuantiaDescansoSemanalRemunerado());
-            eventosService.adicionalNoturno(funcionario.getNivelSalarial(), funcionario.getQuantiaAdicionalNoturno());
-            eventosService.salarioFamilia(funcionario.getNivelSalarial(), funcionario.getQuantidadeDependentes());
-            eventosService.diariaViagem(funcionario.getNivelSalarial(), funcionario.getQuantiaDiariaViagem());
-            eventosService.auxilioCrecheBaba(funcionario.getDataDeNascimento(), funcionario.getQuantidadeDependentes());
 
-            // Obter o valor total dos eventos
-            Float valorTotalEventos = eventosService.getValorTotalEventos();
+@Service
+public class FuncionarioService {
 
-            // Calcular o salário líquido
-            Float salarioLiquido = funcionario.getNivelSalarial() + valorTotalEventos;
+    @Autowired
+    private EventosService eventosService;
+   
+    
+    public List<Funcionario> carregarFuncionarios() throws IOException {
 
-            // Salvar o salário líquido no funcionário
-            funcionario.setSalarioLiquido(salarioLiquido);
-        }
+        ObjectMapper mapper = new ObjectMapper();
+        List<Funcionario> funcionarios = mapper.readValue(new File("resources/funcionarios.json"), new TypeReference<List<Funcionario>>(){});
+        return funcionarios;
+    
+      }
 
-        public void executarEvento(Funcionario funcionario, Dependente dependente) {
-            // Verifica se o objeto de eventos do funcionário não é nulo
-            if (funcionario.getEventos() != null) {
-                // Obtém o objeto de eventos do funcionário
-                Eventos eventos = funcionario.getEventos();
-                
-                // Verifica e executa cada tipo de evento, se estiver presente
-                if (eventos.getHoraExtra50() != null) {
-                    eventosService.adicionarHoraExtra50(funcionario.getNivelSalarial(), eventos.getHoraExtra50());
-                }
-                if (eventos.getHoraExtra100() != null) {
-                    eventosService.adicionarHoraExtra100(funcionario.getNivelSalarial(), eventos.getHoraExtra100());
-                }
-                if (eventos.getFaltas() != null) {
-                    eventosService.subtrairFaltas(funcionario.getNivelSalarial(), eventos.getFaltas());
-                }
-                if (eventos.getAtraso() != null) {
-                    eventosService.descontoPorAtraso(funcionario.getNivelSalarial(), eventos.getAtraso());
-                }
-                if (eventos.getDescansoSemanalRemunerado() != null) {
-                    eventosService.descansoSemanalRemunerado(funcionario.getNivelSalarial(), eventos.getDescansoSemanalRemunerado());
-                }
-                if (eventos.getAdicionalNoturno() != null) {
-                    eventosService.adicionalNoturno(funcionario.getNivelSalarial(), eventos.getAdicionalNoturno());
-                }
-                if (eventos.getSalarioFamilia() != null) {
-                    eventosService.salarioFamilia(funcionario.getNivelSalarial(), funcionario.getQuantidadeDependentes());
-                }
-                if (eventos.getDiariaViagem() != null) {
-                    eventosService.diariaViagem(funcionario.getNivelSalarial(), eventos.getDiariaViagem());
-                }
-                if (eventos.getAuxilioCrecheBaba() != null) {
-                    eventosService.auxilioCrecheBaba(dependente.getDataDeNascimento(), funcionario.getQuantidadeDependentes());
-                }
-            } else {
-                System.out.println("O objeto de eventos do funcionário está nulo.");
-            }
-        }
-        
+    public void calcularSalarioLiquido(Funcionario funcionario) {
+        // Resetar o valor total dos eventos antes de calcular
+        eventosService.resetarValorTotalEventos();
+
+        // Executar eventos para o funcionário
+        eventosService.adicionarHoraExtra50(funcionario.getNivelSalarial(), funcionario.getQuantiaHoraExtra50());
+        eventosService.adicionarHoraExtra100(funcionario.getNivelSalarial(), funcionario.getQuantiaHoraExtra100());
+        eventosService.subtrairFaltas(funcionario.getNivelSalarial(), funcionario.getQuantiaFaltas());
+        eventosService.descontoPorAtraso(funcionario.getNivelSalarial(), funcionario.getQuantiaDescontoPorAtraso());
+        eventosService.descansoSemanalRemunerado(funcionario.getNivelSalarial(),
+                funcionario.getQuantiaDescansoSemanalRemunerado());
+        eventosService.adicionalNoturno(funcionario.getNivelSalarial(), funcionario.getQuantiaAdicionalNoturno());
+        eventosService.salarioFamilia(funcionario.getNivelSalarial(), funcionario.getQuantidadeDependentes());
+        eventosService.diariaViagem(funcionario.getNivelSalarial(), funcionario.getQuantiaDiariaViagem());
+        eventosService.auxilioCrecheBaba(funcionario.getDataDeNascimento(), funcionario.getQuantidadeDependentes());
+
+        // Obter o valor total dos eventos
+        Float valorTotalEventos = eventosService.getValorTotalEventos();
+
+        // Calcular o salário líquido
+        Float salarioLiquido = funcionario.getNivelSalarial() + valorTotalEventos;
+
+        // Salvar o salário líquido no funcionário
+        funcionario.setSalarioLiquido(salarioLiquido);
     }
+
+    public void executarEvento(Funcionario funcionario, Dependente dependente) {
+        // Verifica se o objeto de eventos do funcionário não é nulo
+        if (funcionario.getEventos() != null) {
+            // Obtém o objeto de eventos do funcionário
+            Eventos eventos = funcionario.getEventos();
+
+            // Verifica e executa cada tipo de evento, se estiver presente
+            if (eventos.getHoraExtra50() != null) {
+                eventosService.adicionarHoraExtra50(funcionario.getNivelSalarial(), eventos.getHoraExtra50());
+            }
+            if (eventos.getHoraExtra100() != null) {
+                eventosService.adicionarHoraExtra100(funcionario.getNivelSalarial(), eventos.getHoraExtra100());
+            }
+            if (eventos.getFaltas() != null) {
+                eventosService.subtrairFaltas(funcionario.getNivelSalarial(), eventos.getFaltas());
+            }
+            if (eventos.getAtraso() != null) {
+                eventosService.descontoPorAtraso(funcionario.getNivelSalarial(), eventos.getAtraso());
+            }
+            if (eventos.getDescansoSemanalRemunerado() != null) {
+                eventosService.descansoSemanalRemunerado(funcionario.getNivelSalarial(),
+                        eventos.getDescansoSemanalRemunerado());
+            }
+            if (eventos.getAdicionalNoturno() != null) {
+                eventosService.adicionalNoturno(funcionario.getNivelSalarial(), eventos.getAdicionalNoturno());
+            }
+            if (eventos.getSalarioFamilia() != null) {
+                eventosService.salarioFamilia(funcionario.getNivelSalarial(), funcionario.getQuantidadeDependentes());
+            }
+            if (eventos.getDiariaViagem() != null) {
+                eventosService.diariaViagem(funcionario.getNivelSalarial(), eventos.getDiariaViagem());
+            }
+            if (eventos.getAuxilioCrecheBaba() != null) {
+                eventosService.auxilioCrecheBaba(dependente.getDataDeNascimento(),
+                        funcionario.getQuantidadeDependentes());
+            }
+        } else {
+            System.out.println("O objeto de eventos do funcionário está nulo.");
+        }
+    }
+
+}
