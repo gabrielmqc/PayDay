10cb431effc4fe442cab02ddea8c0bcf49e7d62a
Gabriel Mazzo <gabriel_mqc@hotmail.com>
2024-04-16 22:08:57 -0300

    att

--- Payday/PayDay/demo/PayDay/src/main/java/com/payment/demo/controller/EmpregadorController.java
+++ Payday/PayDay/demo/PayDay/src/main/java/com/payment/demo/controller/EmpregadorController.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.File;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping("/empregadores")
 public class EmpregadorController {
 
--- Payday/PayDay/demo/PayDay/src/main/java/com/payment/demo/controller/FuncionarioController.java
+++ Payday/PayDay/demo/PayDay/src/main/java/com/payment/demo/controller/FuncionarioController.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -19,6 +20,7 @@ import com.payment.demo.service.FuncionarioService;
 
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping("/funcionarios")
 public class FuncionarioController {
 
